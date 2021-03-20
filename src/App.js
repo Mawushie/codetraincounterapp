b6228252662f9a671f1333723f22c7476661a830
Mawushie <afi.mawushie.honyah@gmail.com>
2021-03-20 16:27:21 +0000

    counter app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+
+// class App extends React.Component{
+//   constructor(){
+//     super()
+//     this.state = {age: 20};
+//   }
+//   handleClick = () =>{
+//     // console.log("button clicked")
+//     let newAge = this.state.age + 1;
+//     this.setState({age: newAge});
+//   }
+//   render(){
+//     return (
+//       <div style={ {marginLeft: 10} }>
+//         <h3>Age is {this.state.age}</h3>
+//         <button onClick = {this.handleClick}> Click me!!!</button>
+//       </div>
+//     )
+//   } 
+// };
+
 
 function App() {
+  const [state, setState] = useState( {age:20} );
+
+
+  const handleClick = () => {
+    let newAge = state.age +1;
+    setState({age : newAge});
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={ {marginLeft: 10} }>
+      <h3>Age is {state.age}</h3>
+      <button onClick = {handleClick}> Click me!!!</button>
     </div>
   );
 }
